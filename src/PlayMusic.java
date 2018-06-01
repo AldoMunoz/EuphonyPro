78eef53122b85ffb98db5451b48f5f1167af10f4
AldoMunoz <aldothenal@gmail.com>
2018-06-01 11:13:11 -0400

    Commit Message

--- src/PlayMusic.java
+++ src/PlayMusic.java
@@ -8,7 +8,8 @@ import java.io.InputStream;
 
 public class PlayMusic {
     public static void main(String[] args) {
-
+        playMusic("fortnitetest.wav");
+        System.out.println("Running");
     }
 
     public static void playMusic(String filepath) {
