74e9694a7d6db9ee3c6ee95589b6ec3973e1666e
jakevalencia <jakval21@bergen.org>
2018-06-05 13:56:33 -0400

    this is test code that needs to be editted

--- src/HomeScreen.java
+++ src/HomeScreen.java
@@ -8,14 +8,18 @@ public class HomeScreen extends JFrame {
     public HomeScreen(){
         JPanel homeScreen = new JPanel();
         JPanel greetingLabel = new JPanel();
-        JPanel pianoOption = new JPanel();
+        JPanel option = new JPanel();
+        option.setLayout(new GridLayout(2,1));
+
         //ImageIcon guitarPicture = new ImageIcon("o.gif");
         ImageIcon pianoPicture = new ImageIcon("CompSciPiano.jpg");
+        ImageIcon trumpetPicture = new ImageIcon("trumpet.jpeg");
 
-        JLabel welcomeBanner = new JLabel("Welcome to Euphony");
-        JLabel chooseInstrument = new JLabel("Pick your instument");
+        JLabel welcomeBanner = new JLabel("Welcome to Euphony!");
+        JLabel chooseInstrument = new JLabel("Pick your instrument:");
 
         JButton piano = new JButton("Piano", pianoPicture);
+        JButton trumpet = new JButton("Trumpet", trumpetPicture);
         piano.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -27,20 +31,32 @@ public class HomeScreen extends JFrame {
             }
         });
 
+        trumpet.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Trumpet_Screen frame2 = new Trumpet_Screen();
+                frame2.setSize(1000, 1000);
+                frame2.setLocationRelativeTo(null);
+                frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame2.setVisible(true);
+            }
+        });
+
 
         homeScreen.add(welcomeBanner);
         greetingLabel.add(chooseInstrument);
-        pianoOption.add(piano);
+        option.add(piano);
+        option.add(trumpet);
 
         add(homeScreen,  BorderLayout.NORTH);
         add(greetingLabel, BorderLayout.CENTER);
-        add(pianoOption, BorderLayout.EAST);
+        add(option, BorderLayout.EAST);
 
     }
 
     public static void main(String[] args) {
         HomeScreen frame = new HomeScreen();
-        frame.setSize(500, 700);
+        frame.setSize(500, 1000);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
